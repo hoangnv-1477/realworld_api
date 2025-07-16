e722e7cf748536c0d8752b89721c39384bc0f657
hoangnv-1477 <49389627+hoangnv-1477@users.noreply.github.com>
2025-07-16 08:55:06 +0700

    feat: basic user register (#2)

---
+++ api/serializers.py
@@ -0,0 +1,15 @@
+from rest_framework import serializers
+from .models import User
+
+class RegistrationSerializer(serializers.Serializer):
+    username = serializers.CharField(max_length=150, required=True)
+    email = serializers.EmailField(required=True)
+    password = serializers.CharField(write_only=True, required=True)
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            password=validated_data['password']
+        )
+        return user
--- api/views.py
+++ api/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework import viewsets, status
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework.permissions import AllowAny
 
-# Create your views here.
+from .models import User
+from .serializers import RegistrationSerializer
+
+class UserViewSet(viewsets.GenericViewSet):
+    permission_classes = [AllowAny]
+    serializer_class = RegistrationSerializer
+
+    @action(detail=False, methods=['post'], url_path='register')
+    def register(self, request):
+        user_data_from_request = request.data.get('user', {})
+        serializer = self.get_serializer(data=user_data_from_request)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+
+        return Response(
+          {"message": f"User '{user.username}' registered successfully!", "id": user.id},
+          status=status.HTTP_201_CREATED
+        )
