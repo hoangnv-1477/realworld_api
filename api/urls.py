b323a264b0d84f6754c0323bc892087e21af14c4
nguyen.van.hoang <nguyen.van.hoang@sun-asterisk.com>
2025-07-16 15:56:02 +0700

    feat: write login api

--- api/urls.py
+++ api/urls.py
@@ -4,4 +4,5 @@ from .views import UserViewSet
 
 urlpatterns = [
     path('users/', UserViewSet.as_view({'post': 'register'}), name='user-register'),
+    path('users/login', UserViewSet.as_view({'post': 'login'}), name='user-login'),
 ]
