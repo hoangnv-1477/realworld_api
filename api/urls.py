cb1d97580da104d38887cdaff6cbc7cac4dd4461
nguyen.van.hoang <nguyen.van.hoang@sun-asterisk.com>
2025-07-22 14:42:39 +0700

    feat: get all tags

--- api/urls.py
+++ api/urls.py
@@ -1,13 +1,15 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from .views import UserViewSet, ArticleViewSet
+from .views import UserViewSet, ArticleViewSet, TagViewSet
 
 router = DefaultRouter()
 router.register(r'articles', ArticleViewSet, basename='article')
+router.register(r'tags', TagViewSet, basename='tag')
 
 urlpatterns = [
     path('users/', UserViewSet.as_view({'post': 'register'}), name='user-register'),
     path('users/login', UserViewSet.as_view({'post': 'login'}), name='user-login'),
+    path('user', UserViewSet.as_view({'get': 'get_current_user'}), name='user-current'),
     path('', include(router.urls)),
 ]
